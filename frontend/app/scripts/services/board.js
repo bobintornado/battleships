b7067fd8a27cb56f2c2025edb26cdfd4a017452a
Lam Kee Wei <keewei.lam@gmail.com>
2014-03-26 18:09:04 +0800

    Added responsive logo

--- frontend/app/scripts/services/board.js
+++ frontend/app/scripts/services/board.js
@@ -2,6 +2,37 @@
 
 angular.module('frontendApp')
   .factory('Board', function () {
+    var shipLengths = [5,4,3,3,2];
+
+    var getEmptyBoard = function(){
+      var board = [];
+
+      for(var i = 0; i < 7; i++){
+        var row = [];
+      
+        for(var j = 0; j < 7; j++){
+          row.push('-');
+        }
+      
+        board.push(row);
+      }
+
+      return board;
+    };
+
+
+    var getPoint = function(ship, board){
+      
+    };
+
+    var generateBoard = function(){
+      var board = getEmptyBoard();
+      
+      shipLengths.forEach(function(ship){
+        var point = getPoint(ship, board);
+
+      });
+    };
 
     return {
       initializeBoards: function () {

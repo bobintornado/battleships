6f0931d0f447e67ba524f7efecf007a6133d59b2
Lam Kee Wei <keewei.lam@gmail.com>
2014-03-28 01:41:34 +0800

    Built and added sample codes

--- frontend/app/scripts/controllers/bot.js
+++ frontend/app/scripts/controllers/bot.js
@@ -2,6 +2,17 @@
 
 angular.module('frontendApp')
   .controller('BotCtrl', function ($scope, Bot) {
+    var getSample = function(language){
+      switch(language){
+        case 'javascript':
+          return 'function getMove(boardStr) {\n  //Write your code here...\n  return boardStr;\n}';
+        case 'java':
+          return 'public static void getMove(String boardStr){\n  //Write your code here...\n  return boardStr;\n}';
+        case 'python':
+          return 'def getMove(boardStr):\n  //Write your code here\n  return boardStr';
+      }
+    };
+
     $scope.playerOptions = {
       lineWrapping : true,
       lineNumbers: true,
@@ -27,22 +38,21 @@ angular.module('frontendApp')
 
     $scope.playerBot = {
       name: 'lamkeewei',
-      language: $scope.settings.language
+      language: $scope.settings.language,
+      code: getSample($scope.settings.language)
     };
 
     $scope.computerBot = {
       name: 'lamkeewei',
-      language: $scope.settings.language
+      language: $scope.settings.language,
+      code: getSample($scope.settings.language)
     };
-
-    $scope.playerBot.code = 'function getMove(boardStr) {\n  //Write your code here...\n  return boardStr;\n}';
-
-    $scope.computerBot.code = 'function getMove(boardStr) {\n  //Write your code here...\n  return boardStr;\n}';
 
     $scope.$watch('settings.language', function(newVal, oldVal){
       $scope.botOptions.mode = newVal === 'java' ? 'clike' : newVal;
       $scope.playerOptions.mode = newVal === 'java' ? 'clike' : newVal;
       $scope.playerBot.language = newVal;
+      $scope.playerBot.code = getSample(newVal);
       $scope.computerBot.language = newVal;
     });
 
@@ -50,6 +60,6 @@ angular.module('frontendApp')
       // Bot.saveBot($scope.playerBot).then(function(data){
       //   console.log(data);
       // });
-      console.log($scope.playerBot);
+      console.log($scope.botOptions.mode);
     };
   });

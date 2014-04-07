a9f22aac7c10669f6f8be5c6715fbd460295dd23
Lam Kee Wei <keewei.lam@gmail.com>
2014-04-07 21:02:43 +0800

    Added travis test

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -17,10 +17,15 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/codemirror/lib/codemirror.js',
+      'app/bower_components/angular-ui-codemirror/ui-codemirror.js',
+      'app/bower_components/angular-local-storage/angular-local-storage.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      // 'test/mock/**/*.js',
+      // 'test/spec/**/*.js'
+      'test/spec/controllers/board.js'
     ],
 
     // list of files / patterns to exclude
--- frontend/test/spec/controllers/board.js
+++ frontend/test/spec/controllers/board.js
@@ -16,7 +16,47 @@ describe('Controller: BoardCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('isShip to return true when the value is "s" and the gameStart is true', function () {
+    scope.settings = {
+      gameStart: true
+    };
+    var val = 's';
+    expect(scope.isShip(val)).toBe(true);
+  });
+
+  it('isShip should not return true when the value is "s" and but gameStart is false', function () {
+    scope.settings = {
+      gameStart: false
+    };
+    var val = 's';
+    expect(scope.isShip(val)).toBe(false);
+  });
+
+  it('isShip should not return true when the value is not "s" and gameStart is false', function () {
+    scope.settings = {
+      gameStart: false
+    };
+    var val = 'x';
+    expect(scope.isShip(val)).toBe(false);
+  });
+
+  it('isMiss to return true when the value is "m"', function () {
+    var val = 'm';
+    expect(scope.isMiss(val)).toBe(true);
+  });
+
+  it('isMiss should not return true when the value is not "m"', function () {
+    var val = 'h';
+    expect(scope.isMiss(val)).toBe(false);
+  });
+
+  it('isHit to return true when the value is "h"', function () {
+    var val = 'h';
+    expect(scope.isHit(val)).toBe(true);
+  });
+
+  it('isHit should not return true when the value is not "h"', function () {
+    var val = 'm';
+    expect(scope.isHit(val)).toBe(false);
   });
 });
